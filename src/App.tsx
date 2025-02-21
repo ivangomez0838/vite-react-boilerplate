21ae7a79d18e8eb62df137738e2aadeb10ed5027
Ivan Gomez Medina <ivangomez0838@gmail.com>
2025-02-20 21:10:12 -0400

    configuracion inicial proyecto de boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { Button } from './components/components/ui/button'
+
+function App() {
+
+  const showMsj = () =>{
+    alert("buena suerte!");
+  }
+  return (
+    <div className='flex flex-col justify-center items-center gap-8 min-h-screen'>
+     <h1 className='text-5xl font-bold text-blue-600 text-center uppercase'>Proyecto boilerplate</h1>
+     <p className='text-muted-foreground'>Sirve como base para iniciar un proyecto utilizando vite + react + typescript + react-query + tailwind css + schadcn</p>
+     <Button onClick={showMsj}>mostrar msj</Button> 
+    </div>
+  )
+}
+
+export default App
